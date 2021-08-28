74dd37e33ae8c5a755f2aba2bfd394d77171976e
Sanjit Sarkar <ssanjit856@gmail.com>
2021-08-28 19:50:33 +0530

    added translation functionality

---
+++ js/app.js
@@ -0,0 +1,18 @@
+
+const translateToBanana = async() => {
+    const message = document.querySelector('.message').value
+    const result = document.querySelector('.result')
+
+    let response = await fetch(`https://api.funtranslations.com/translate/minion.json?text=${message}`)
+    response = await response.json()
+    // console.log(response.contents.translated)
+    result.textContent = response.contents.translated
+
+}
+const translateButton = document.querySelector('.translate').addEventListener("click",async()=>{
+    // alert('hello')
+await translateToBanana()
+
+})
+
+// translateButton()
